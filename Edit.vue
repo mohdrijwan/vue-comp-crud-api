34365294d2ef1c92938eb28b06ed3c5fb636547e
mohdrijwan <52716117+mohdrijwan@users.noreply.github.com>
2023-03-18 07:25:18 +0530

    Add files via upload
    ui created

---
+++ Edit.vue
@@ -0,0 +1,28 @@
+<script setup>
+function handleUpdateStudentForm(){
+  console.log("Updated");
+}
+</script>
+
+<template>
+  <div class="shadow-md pb-6">
+    <div class="bg-indigo-600 p-4">
+      <h1 class="text-3xl font-bold text-center text-white">
+        Edit Student
+      </h1>
+    </div>
+    <form @submit.prevent="handleUpdateStudentForm" class="w-full">
+      <div class="flex-items-center m-6">
+        <div class="w-1/5">
+          <label for="stuid" class="font-medium"> ID : </label>
+        </div>
+      </div>
+    </form>
+  </div>
+<h1 class="text-3xl font-bold underline">
+    Edit
+  </h1>
+</template>
+
+<style scoped>
+</style>
---
+++ View.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <div class="shadow-md pb-6">
+    <table class="table-auto w-full">
+      <thead class="bg-slate-600 text-white">
+        <tr>
+          <th class="py-1">ID</th>
+          <th class="py-1">Name</th>
+          <th class="py-1">Email</th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <tr>
+          <td class="py-2">1</td>
+          <td class="py-2">Rijwan</td>
+          <td class="py-2">rijwan@gmail.com</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="m-8 flex justify-center">
+      <RouterLink :to="{name : 'list'}">
+        <button type="button" class="bg-emerald-700 text-white font-medium py-2 rounded-md px-6 hover:bg-emerald-800 mr-6">
+          Back to Home
+        </button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
