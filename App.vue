ff99d6d528947cc4ff03723b7566850a60edf83a
mohdrijwan <52716117+mohdrijwan@users.noreply.github.com>
2023-03-18 07:29:42 +0530

    Add files via upload
    only ui code

---
+++ App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {RouterView} from "vue-router";
+</script>
+
+<template>
+  <div class="bg-green-600 p-5">
+    <h1 class="text-3x1 md:text-5xl font-bold text-center text-white">
+      Vue3 Composition CRUD with API call
+    </h1>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
